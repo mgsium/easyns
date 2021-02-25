ffcdc199f9c880cad551680ae13de46f1ba691a5
Musab Guma'a <musabgumaa@gmail.com>
2021-02-25 22:27:04 +0000

    Fixed sever crashes.

--- server.ts
+++ server.ts
@@ -9,7 +9,10 @@ app.options("*", cors({
     origin: "*"
 }));
 
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
+
+import routes from "./routes/index";
+routes(app)
 
 app.listen(PORT, () => {
     console.log(`We are live on port ${PORT}`);
